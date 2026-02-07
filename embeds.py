a162f7e3a4719a0bb2a3bd92259ec0da5d4ff831
zeus9990 <86179151+zeus9990@users.noreply.github.com>
2026-02-07 12:00:07 +0530

    Update embeds.py

--- embeds.py
+++ embeds.py
@@ -6,7 +6,7 @@ embed_list = [
         title="📋 Chapter 1: Introduction to Architecture (Xnode & XnodeOS)",
         description=("In this chapter you will dive into the core structure of OpenxAI a decentralized, container-based framework running on the immutable and reproducible NixOS (XnodeOS).Each Xnode is designed to securely handle critical tasks such as training, inference, and data ingestion in a fully decentralized and tamper-proof environment.\n\n"
         ">>> **How to attend the test:**\n"
-        "• Answer X multiple-choice questions in this chapter.\n"
+        "• Answer 5 multiple-choice questions in this chapter.\n"
         "• Each question has a strict one-minute time limit.\n"
         "• Successfully pass all questions to earn an exclusive role that unlocks the next academy level.\n"
         "• Make sure to carefully study the provided documentation by clicking the \"Read Chapter\" button below before you jump in the test.\n"
@@ -24,7 +24,7 @@ embed_list = [
         description=(
             "In this chapter you will find out how OpenxAI creates a truly decentralized storage solution by storing datasets and models across distributed networks like Openmesh. This ensures data persistence and censorship resistance, while AI models are tokenized as NFTs (ERC-721 / ERC-6551), granting immutable ownership and transparent provenance.\n\n"
             ">>> **How to attend the test:**\n"
-            "• Answer X multiple-choice questions in this chapter.\n"
+            "• Answer 5 multiple-choice questions in this chapter.\n"
             "• Each question has a strict one-minute time limit.\n"
             "• Successfully pass all questions to earn an exclusive role that unlocks the next academy level.\n"
             "• Make sure to carefully study the provided documentation by clicking the \"Read Chapter\" button below before you jump in the test.\n"
@@ -36,5 +36,24 @@ embed_list = [
         icon_url=url
     ).set_image(
         url="https://i.postimg.cc/tCZLDb6Z/Openx-Ai-Academy-LEVEL-2.png"
+    ),
+    discord.Embed(
+        title="📋 Chapter 3: Introduction to Model Layer",
+        description=(
+            "Explore how containerized AI models are hosted on Xnodes, while smart contracts govern licensing, access control, and monetization, empowering permissionless, transparent and fair model usage. This creates a truly decentralized AI compute layer where models operate in a trustless, verifiable manner.\n\n"
+            ">>> **How to attend the test:**\n"
+            "• Answer 5 multiple-choice questions in this chapter.\n"
+            "• Each question has a strict one-minute time limit.\n"
+            "• Successfully pass all questions to earn an exclusive role that unlocks the next academy level.\n"
+            "• Make sure to carefully study the provided documentation by clicking the \"Read Chapter\" button below before you jump in the test.\n"
+            "⚠️ If you answer incorrectly, you must wait **1 hour** before trying again."
+        ),
+        color=color
+    ).set_footer(
+        text="For any help, contact moderators or support.",
+        icon_url=url
+    ).set_image(
+        url="https://i.postimg.cc/SsSBGzLk/OpenxAi_Academy_-_LEVEL_3.png"
     )
+
 ]
